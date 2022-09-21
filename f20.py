3398f2248881e45bec46e45d24f1c3f94b60e4db
Prem1535 <112494348+Pram1535@users.noreply.github.com>
2022-09-21 15:40:25 +0530

    Add files via upload

---
+++ f20.py
@@ -0,0 +1,10 @@
+def UncommonWords(A,B):
+	count = {}
+	for word in A.split():
+		count[word] = count.get(word, 0) + 1
+	for word in B.split():
+		count[word] = count.get(word, 0) + 1
+	return [word for word in count if count[word] == 1]
+A=input("Enter first scentence = ")
+B=input("Enter second scentence = ")
+print(UncommonWords(A, B))
